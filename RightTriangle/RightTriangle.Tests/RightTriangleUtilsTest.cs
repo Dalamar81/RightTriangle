4b7f84fdcfbfbf9a71e60a0c8aa0a5a983e29cc1
Суходько Александр <dalamar81@gmail.com>
2015-11-24 23:02:00 +0300

    Начальный коммит

---
+++ RightTriangle/RightTriangle.Tests/RightTriangleUtilsTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RightTriangle.Tests
+{
+    [TestClass]
+    public class RightTriangleUtilsTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestPositiveLengthA()
+        {
+            RightTriangleUtils.CalculateSquare(-1, 2, 3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestPositiveLengthB()
+        {
+            RightTriangleUtils.CalculateSquare(1, -2, 3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestPositiveLengthC()
+        {
+            RightTriangleUtils.CalculateSquare(1, 2, -3);
+        }
+    }
+}
---
+++ RightTriangle/RightTriangle/RightTriangleUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RightTriangle
+{
+    public static class RightTriangleUtils
+    {
+        /// <summary>
+        /// Вычисляет площадь прямоугольного треугольника
+        /// </summary>
+        /// <param name="a">Длина стороны a</param>
+        /// <param name="b">Длина стороны b</param>
+        /// <param name="c">Длина стороны c</param>
+        public static double CalculateSquare(double a, double b, double c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
